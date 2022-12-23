720d9bf9adc8e989a741640c76ac34d8463359ce
Hugh He <46068081+plushugh@users.noreply.github.com>
2022-12-23 01:35:19 +0000

    v0.1.0

--- src/cli.ts
+++ src/cli.ts
@@ -2,19 +2,10 @@
 
 import program from 'commander'
 
-import { orderPizza } from './index'
- 
+import { processFile } from './index'
+
 program
   .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv)
 
-orderPizza({
-  peppers: program.peppers,
-  pineapple: program.pineapple,
-  bbqSauce: program.bbqSauce,
-  cheeseType: program.cheese
-}).then(result => console.log(result.message))
+processFile(program.args[0], program.args[1])
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,72 @@
-export interface Toppings {
-  peppers: boolean
-  pineapple: boolean
-  bbqSauce: boolean
-  cheeseType: string
+import csv from "csv-parser";
+import * as fs from "fs";
+
+interface TestSession {
+  examdate: string;
+  examtime: string;
+  subject: string;
+  grade: string;
+  classNum: string;
+  minutes: string;
+  groupChatName: string;
+  numberOfPeople: string;
+  proctor1: string;
+  proctor2: string;
+  teacher: string;
+  students: string[];
 }
 
-export async function orderPizza (toppings: Toppings): Promise<{ message: string}> {
-  let message = 'you ordered a pizza with:\n'
-  if (toppings.peppers) message += '  - peppers\n'
-  if (toppings.pineapple) message += '  - pineapple\n'
-  if (toppings.bbqSauce) message += '  - bbq\n'
-  message += `  - ${toppings.cheeseType} cheese`
-  return {
-    message
-  }
+export function processFile(inputFilename: string, outputFilename: string) {
+  const testSessionsByStudent: { [student: string]: TestSession[] } = {};
+
+  fs.createReadStream(inputFilename, { encoding: "utf-8" })
+    .pipe(csv())
+    // for each row, add the test session to the student's list of test sessions
+    .on("data", (row) => {
+      console.log(row);
+      const examdate = row["examdate"];
+      const examtime = row.examtime;
+      const subject = row.subject;
+      const grade = row.grade;
+      const classNum = row.classnum;
+      const minutes = row.minutes;
+      const groupChatName = row.group_chat_name;
+      const numberOfPeople = row.number_of_people;
+      const proctor1 = row.proctor1;
+      const proctor2 = row.proctor2;
+      const teacher = row.teacher;
+      const students = Object.values(row).slice(11); // slice off the first 11 columns to get the student names
+
+      for (const student of <string[]>students) {
+        if (!testSessionsByStudent[student]) {
+          testSessionsByStudent[student] = [];
+        }
+
+        testSessionsByStudent[student].push({
+          examdate,
+          examtime,
+          subject,
+          grade,
+          classNum,
+          minutes,
+          groupChatName,
+          numberOfPeople,
+          proctor1,
+          proctor2,
+          teacher: teacher.endsWith("、examaffairs@brs.edu.cn")
+            ? teacher.slice(0, -23)
+            : teacher,
+          students: <string[]>(
+            students.filter((str: string) => str.trim() !== "")
+          ),
+        });
+      }
+    })
+    .on("end", () => {
+      // output it as a JSON file
+      fs.writeFileSync(
+        outputFilename,
+        JSON.stringify(testSessionsByStudent, null, 2)
+      );
+    });
 }
