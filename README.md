d51f9585aafa4523ee86f11679659721b20d2dbd
Hugh He <plushugh@gmail.com>
2023-06-28 16:59:23 +0800

    refactor to deno, ui updates

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Usage
 
 ## Delete header rows in excel
+
 ## Spread merged cells using VBA script
 
 1. Alt + F11
 2. Insert > Module
 3. Add this script
+
 ```vba
 Sub UnMergeSameCell()
-'Upadateby Extendoffice
 Dim Rng As Range, xCell As Range
 xTitleId = "KutoolsforExcel"
 Set WorkRng = Application.Selection
@@ -28,31 +29,38 @@ Application.ScreenUpdating = True
 End Sub
 ```
 <!-- credit: Extendoffice -->
-4. Select all cells
-5. Use F5 to run the script
-6. Export csv
+1. Select all cells in sheet
+2. Use F5 to run the script
+3. Export csv as `test.csv`
 
-# Open csv and add column names
+## Open csv and add column names
+
 1. Open csv in textedit
 2. add this to an extra top line:
 
-```
-examdate,examtime,subject,classnum,minutes,location,proctor1,proctor2,number_of_people,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42,s43,s44,s45
+```csv
+examDate,examTime,subject,classNum,minutes,location,proctor1,proctor2,numOfStudents,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42,s43,s44,s45
 ```
 
-3. rename csv as test.csv and copy to this folder
+1. rename csv as `source.csv` and copy to this folder
 
 ## Strip BOM from csv file
-### note input file is named `test.csv`
+
 ```bash
 npm install
 npm run strip-bom
 ```
 
+> this npm script generates a new file `input.csv`
+
 ## Generate preindexed json
+
 ```bash
-npm run generate testbom.csv public/testSessionsByStudent.json
+npm run generate-json
 ```
 
+> this npm script reads `input.csv` and generates a new file in `public/` as `testSessionsByStudent.json`
+
 ## Deploy webapp from `public/` directory
-Netlify
+
+Use netlify, since this app uses netlify forms for form collection.
--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,86 @@
-#!/usr/bin/env node
+import { parse } from "https://deno.land/std@0.192.0/csv/mod.ts";
 
-import program from 'commander'
+interface TestSession {
+  examDate: string;
+  examTime: string;
+  subject: string;
+  classNum: string;
+  minutes: string;
+  location: string;
+  proctor1: string;
+  proctor2: string | null;
+  numOfStudents: string;
+  students: string[];
+}
 
-import { processFile } from './index'
+export async function processFile(
+  inputFilename: string,
+  outputFilename: string
+) {
+  const testSessionsByStudent: Record<string, TestSession[]> = {};
 
-program
-  .version('0.1.0')
-  .parse(process.argv)
+  try {
+    const file = await Deno.readTextFile(inputFilename);
+    const csv = parse(file, { trimLeadingSpace: true, skipFirstRow: true }); // Skip the first row of the CSV file (headers) and trim leading spaces, the first row will then be used as the keys for the returned object
 
-processFile(program.args[0], program.args[1])
+    for await (const row of csv) {
+      const {
+        examDate,
+        examTime,
+        subject,
+        classNum,
+        minutes,
+        location,
+        proctor1,
+        proctor2,
+        numOfStudents,
+        ...students
+      } = row;
+
+      const studentNames: string[] = Object.values(students).filter(
+        Boolean
+      ) as string[];
+
+      studentNames.forEach((student) => {
+        if (!testSessionsByStudent[student]) {
+          testSessionsByStudent[student] = [];
+        }
+
+        testSessionsByStudent[student].push({
+          examDate: examDate || "INVALID DATE",
+          examTime: examTime || "INVALID TIME",
+          subject: subject || "INVALID SUBJECT",
+          classNum: classNum || "INVALID CLASS NUMBER",
+          minutes: minutes || "INVALID MINUTES",
+          location: location || "INVALID LOCATION",
+          proctor1: proctor1 || "INVALID PROCTOR",
+          proctor2: proctor2 || null,
+          numOfStudents: numOfStudents || "INVALID NUMBER OF STUDENTS",
+          students: studentNames,
+        });
+      });
+    }
+
+    // const jsonData = JSON.stringify(testSessionsByStudent, null, 2);
+    const jsonData = JSON.stringify(testSessionsByStudent); // Saves precious bytes
+
+    Deno.writeTextFile(outputFilename, jsonData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  const inputFilename = Deno.args[0];
+  const outputFilename = Deno.args[1];
+
+  if (!inputFilename || !outputFilename) {
+    console.error(
+      "Please provide an input and output filename. Example: deno run --allow-read --allow-write src/cli.ts input.csv public/testSessionsByStudent.json"
+    );
+    Deno.exit(1);
+  }
+
+  await processFile(inputFilename, outputFilename);
+}
